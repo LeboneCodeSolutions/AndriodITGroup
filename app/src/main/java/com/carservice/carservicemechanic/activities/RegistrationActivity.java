a15d0a7456bacdf7aa26ba046da5ac17d6922a43
masterlebone <68324851+masterlebone@users.noreply.github.com>
2025-11-07 16:45:05 +0200

    Added Registration form and the activity_register.xml

---
+++ app/src/main/java/com/carservice/carservicemechanic/activities/RegistrationActivity.java
@@ -0,0 +1,17 @@
+package com.carservice.carservicemechanic.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.carservice.carservicemechanic.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+
+
+}
+
